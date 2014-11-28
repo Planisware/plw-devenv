4e7ab53ba4917a769c9be7f2cc7676b12a6c12fc
Mathieu Gautier <mathieu.gautier@planisware.com>
2014-11-28 17:59:50 +0000

    add auto-complete for alisp  (header added automatically)

--- el/ac-alisp.el
+++ el/ac-alisp.el
@@ -1,4 +1,45 @@
-;;; DOC add auto-complete for alisp
+;;;; -*- coding: windows-1252 -*-
+;;;; COPYRIGHT (C) PLANISWARE $Date$ 
+;;;;
+;;;; All Rights Reserved
+;;;;
+;;;; This program and the information contained herein are confidential to
+;;;; and the property of PLANISWARE and are made available only to PLANISWARE
+;;;; employees for the sole purpose of conducting PLANISWARE business.
+;;;;
+;;;; This program and copy therof and the information contained herein shall
+;;;; be maintained in strictest confidence ; shall not be copied in whole or
+;;;; in part except as authorized by the employee's manager ; and shall not
+;;;; be disclosed or distributed (a) to persons who are not PLANISWARE employees,
+;;;; or (b) to PLANISWARE employees for whom such information is not necessary in
+;;;; connection with their assigned responsabilities.
+;;;;
+;;;; There shall be no exceptions to the terms and conditions set forth
+;;;; herein except as authorized in writing by the responsible PLANISWARE General
+;;;; Manager.
+
+;;;;
+;;;; FILE    : $RCSfile$
+;;;;
+;;;; AUTHOR  : $Author$
+;;;;
+;;;; VERSION : $Id$
+;;;;
+;;;; PURPOSE :
+;;;;
+;;;; (when (fboundp :set-source-info) (:set-source-info "$RCSfile$" :id "$Id$" :version "$Revision$" :date "$Date$ "))
+;;;; (when (fboundp :doc-patch) (:doc-patch ""))
+;;;; (:require-patch "")
+;;;; HISTORY :
+;;;; $Log$
+;;;; Revision 3.1  2014/11/28 17:59:50  mgautier
+;;;; add auto-complete for alisp
+;;;;  (header added automatically)
+;;;;
+
+;;(when (fboundp :require-patch) (:require-patch ""))
+(when (fboundp :set-source-info) (:set-source-info "$RCSfile$" :id "$Id$" :version "$Revision$" :date "$Date$"))
+(when (fboundp :doc-patch) (:doc-patch "add auto-complete for alisp"))
 
 (require 'auto-complete)
 ;(require 'pos-tip)
