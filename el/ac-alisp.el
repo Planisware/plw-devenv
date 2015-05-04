35f299add8661f12383067089b5c0919640ef00c
Thibault Roche <thibault.roche@planisware.com>
2015-05-04 14:37:04 +0000

    * display the package only in the documentation

--- el/ac-alisp.el
+++ el/ac-alisp.el
@@ -32,6 +32,9 @@
 ;;;; (:require-patch "")
 ;;;; HISTORY :
 ;;;; $Log$
+;;;; Revision 3.5  2015/05/04 14:37:04  troche
+;;;; * display the package only in the documentation
+;;;;
 ;;;; Revision 3.4  2015/01/21 16:25:07  mgautier
 ;;;; - add filename completion in alisp's modes
 ;;;;
@@ -54,142 +57,131 @@
 (require 'auto-complete nil 'noerror)
 ;(require 'pos-tip)
 
-(when (featurep 'auto-complete)
-(progn
- 
+(defvar *ac-current-candidates* nil)
 
-  (ac-define-source "alisp-functions"
-    '((candidates . alisp-functions-ac-candidates)
-      (document . alisp-functions-ac-document)
-      ;(summary . alisp-ac-summary)
-      ;(prefix . alixsp-ac-prefix)
-      (action . alisp-function-ac-action)
-      (symbol . "al")
-      (requires . -1)))
+(ac-define-source "alisp-functions"
+  '((candidates . alisp-functions-ac-candidates)
+    (document . alisp-functions-ac-document)
+					;(summary . alisp-ac-summary)
+					;(prefix . alixsp-ac-prefix)
+    (action . alisp-function-ac-action)
+    (symbol . "al")
+    (requires . -1)))
 
-  (defun alisp-functions-ac-document (symbol)
-    ""
-    symbol)
-  
-  (defun alisp-ac-prefix ()
-    ""
-    "")
-  
-  (defun alisp-function-ac-action()
-    (delete-region (- (point) (length candidate)) (point))
-    (insert 
-     (save-match-data
-       (let ((str candidate))
-	 (string-match "[^(]*(\\([^)]*\\))" str)
-	 (match-string 1 str))))
-					;  (insert "toto")
-    )
-  
-  (let ((str "azeaze (ertre)"))
-    (string-match "[^(]*(\([^)]*\))" str))
-  
-  (save-match-data
-    (let ((str "azeaze (ertre)"))
-      (string-match "[^(]*(\\([^)]*\\))" str)
-      (match-string 1 str)))
-  
+(defun alisp-functions-ac-document (symbol)
+  symbol)
 
+(defun alisp-ac-prefix ()
+  ""
+  "")
 
+(defun alisp-function-ac-action()
+  ;; TODO : insert the symbol when needed
+  ;; get the package : everything before the first :
+  (save-excursion
+    (save-match-data 
+      (let* ((full-candidate (cdr (assoc candidate *ac-current-candidates*)))
+	     (pos (string-match ":" full-candidate))
+	     (package (when (and (numberp pos) (> pos 0)) (substring full-candidate 0 pos))))
+	;; insert the package at the right position
+	(when package 
+	  (backward-char (length candidate))
+	  ;; check that the package is not already there	  	  
+	  (unless (equal (buffer-substring-no-properties (point) (+ (point) (length package) 1))
+			 (format "%s:" package))
+	    (insert package)
+	    (insert "::")))))))
 
-  (defun as-string(alist)
-    (mapcar (lambda (s) (symbol-name (cdr s))) alist))
-  
-  (defun as-string2(alist)
-    (mapcar 'symbol-name (mapcar 'cdr alist)))
-  
-  ;; see eli/fi-lep.el in allegro lisp install directory
-  (defun alisp-functions-ac-candidates ()
-    (interactive)
-    (with-local-quit
-      (let* ((end (point))
-	     xpackage real-beg
-	     (beg (save-excursion
-		    (backward-sexp 1)
-		    (while (= (char-syntax (following-char)) ?\')
-		      (forward-char 1))
-		    (setq real-beg (point))
-		    (let ((opoint (point)))
-		      (if (re-search-forward ":?:" end t)
-			  (setq xpackage
-				(concat
-				 ":"
-				 (fi::defontify-string
-				  (buffer-substring opoint (match-beginning 0)))))))
-		    (point)))
-	     (pattern (fi::defontify-string (buffer-substring beg end)))
-	     (functions-only (if (eq (char-after (1- real-beg)) ?\() t nil))
-	     (downcase (and (eq ':upper fi::lisp-case-mode)
-			    (not (fi::all-upper-case-p pattern))))
-	     (xxalist (fi::lisp-complete-1 pattern xpackage functions-only))
-	     temp
-	     (package-override nil)
-	     (xalist
-	      (if (and xpackage (cdr xxalist))
-		  (fi::package-frob-completion-alist xxalist)
-		(if (and (not xpackage)
-			 ;; current package of buffer is not the same as the
-			 ;; single completion match
-			 (null (cdr xxalist)) ;; only one
-			 (setq temp (fi::extract-package-from-symbol
-				     (cdr (car xxalist))))
-			 (not
-			  (string= (fi::full-package-name
-				    (or (fi::package) "cl-user"))
-				   (fi::full-package-name temp))))
-		    (progn
-		      (setq package-override t)
-		      xxalist)
-		  xxalist)))
-	     (alist (if downcase
-			(mapcar 'fi::downcase-alist-elt xalist)
-		      xalist))
-	     (completion
-	      (when alist
-		(let* ((xfull-package-name
-			(if (string= ":" xpackage)
-			    "keyword"
-			  (when xpackage
-			    (fi::full-package-name xpackage))))
-		       (full-package-name
-			(when xfull-package-name
-			  (if downcase
-			      (downcase xfull-package-name)
-			    xfull-package-name))))
-		  (when (or full-package-name package-override)
-		    (setq pattern
-			  (format "%s::%s" full-package-name pattern)))
-		  (try-completion pattern alist)))))
-					;       (message "%s" alist)
-					;       alist)))
-	(sort (mapcar (lambda (v) (concatenate 'string (car v) "\t\t\t (" (cdr v) ")" )) alist) #'string-lessp))))
-					;      (mapcar 'car alist))))
-  
+;; see eli/fi-lep.el in allegro lisp install directory
+(defun alisp-functions-ac-candidates ()
+  (interactive)
+  ;; reset packages
+  (setq *ac-current-candidates* nil)
+
+  (with-local-quit
+    (let* ((end (point))
+	   xpackage real-beg
+	   (beg (save-excursion
+		  (backward-sexp 1)
+		  (while (= (char-syntax (following-char)) ?\')
+		    (forward-char 1))
+		  (setq real-beg (point))
+		  (let ((opoint (point)))
+		    (if (re-search-forward ":?:" end t)
+			(setq xpackage
+			      (concat
+			       ":"
+			       (buffer-substring-no-properties opoint (match-beginning 0))))))
+		  (point)))
+	   (pattern (buffer-substring-no-properties beg end))
+	   (functions-only (if (eq (char-after (1- real-beg)) ?\() t nil))
+	   (downcase (and (eq ':upper fi::lisp-case-mode)
+			  (not (fi::all-upper-case-p pattern))))
+	   (xxalist (fi::lisp-complete-1 pattern xpackage functions-only))
+	   temp
+	   (package-override nil)
+	   (xalist
+	    (if (and xpackage (cdr xxalist))
+		(fi::package-frob-completion-alist xxalist)
+	      (if (and (not xpackage)
+		       ;; current package of buffer is not the same as the
+		       ;; single completion match
+		       (null (cdr xxalist)) ;; only one
+		       (setq temp (fi::extract-package-from-symbol
+				   (cdr (car xxalist))))
+		       (not
+			(string= (fi::full-package-name
+				  (or (fi::package) "cl-user"))
+				 (fi::full-package-name temp))))
+		  (progn
+		    (setq package-override t)
+		    xxalist)
+		xxalist)))
+	   (alist (if downcase
+		      (mapcar 'fi::downcase-alist-elt xalist)
+		    xalist))
+	   (completion
+	    (when alist
+	      (let* ((xfull-package-name
+		      (if (string= ":" xpackage)
+			  "keyword"
+			(when xpackage
+			  (fi::full-package-name xpackage))))
+		     (full-package-name
+		      (when xfull-package-name
+			(if downcase
+			    (downcase xfull-package-name)
+			  xfull-package-name))))
+		(when (or full-package-name package-override)
+		  (setq pattern
+			(format "%s::%s" full-package-name pattern)))
+		(try-completion pattern alist)))))
+      (setq *ac-current-candidates* alist))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  
+
 ;;;;; configuration of the minor mode
-  
-  
-  ;; definition of a new autocomplete mode
-  (defun alisp-setup-auto-complete-mode ()
-    "Setup ac-lisp to be used with auto-complete-mode."
-    (setq ac-sources '(ac-source-alisp-functions ac-source-filename))
+
+
+;; definition of a new autocomplete mode
+(defun alisp-setup-auto-complete-mode ()
+  "Setup ac-lisp to be used with auto-complete-mode."
+  (setq ac-sources '(ac-source-alisp-functions ac-source-filename))
   )
 
-  (add-hook 'fi:lisp-mode-hook 'alisp-setup-auto-complete-mode)
-  (add-hook 'fi:subprocess-mode-hook 'alisp-setup-auto-complete-mode)
-  
-  (require 'auto-complete-config)
-  
-  (ac-set-trigger-key "<backtab>")
-  (add-to-list 'ac-modes 'fi:common-lisp-mode)
-  (add-to-list 'ac-modes 'fi:inferior-common-lisp-mode)
-  (add-to-list 'ac-modes 'fi:lisp-listener-mode)
-  
-  (ac-config-default)
-  (setq ac-auto-start nil)))
+(add-hook 'fi:lisp-mode-hook 'alisp-setup-auto-complete-mode)
+(add-hook 'fi:subprocess-mode-hook 'alisp-setup-auto-complete-mode)
 
+(require 'auto-complete-config)
+
+;;  (ac-set-trigger-key "<backtab>")
+;; always autocomplete on backtab
+(define-key ac-mode-map (read-kbd-macro "<backtab>") #'(lambda () (ac-trigger-key-command t)))
+(add-to-list 'ac-modes 'fi:common-lisp-mode)
+(add-to-list 'ac-modes 'fi:inferior-common-lisp-mode)
+(add-to-list 'ac-modes 'fi:lisp-listener-mode)
+
+(ac-config-default)
+(setq ac-auto-start nil)
+(setq ac-use-menu-map t)
+(setq ac-quick-help-delay 0.1)
