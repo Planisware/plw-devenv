11f561af1eda6cb24469f2ee71137d11743876ef
Thibault Roche <thibault.roche@planisware.com>
2015-12-10 14:51:39 +0000

    * pjs mode

---
+++ el/pjs-mode-syntax.el
@@ -0,0 +1,224 @@
+;; constants
+(defconst pjs-font-lock-constants
+  (js--regexp-opt-symbol
+   '("this"
+     "super"
+     "context"
+     "true"
+     "false"
+     "undefined"
+     )))
+
+;; languages keywords
+(defconst pjs-font-lock-keywords
+  (js--regexp-opt-symbol
+   '("namespace"
+     "class"
+     "with"
+     "where"
+     "order by"
+     "group by"
+     "inverse"
+     "break"
+     "case"
+     "catch"
+     "else"
+     "for"
+     "goto"
+     "if"
+     "instanceof"
+     "new"
+     "return"
+     "switch"
+     "try"
+     "typeof"
+     "var"
+     "while"
+     "foreach"
+     "method"
+     "function"
+     )))
+
+;; ojs types
+(defconst pjs-font-lock-types
+  (js--regexp-opt-symbol
+   '(
+     "int32"
+     "int"
+     "longfloat"
+     "shortfloat"
+     "string"
+     "vector"
+     "boolean"
+     "hashtable"
+     )))
+
+;; method definition
+(defconst *ojs-method-heading*
+"^[ \t]*method[ \t]+\\(\\w+\\)[ \t]+\\(\\<on\\>\\)[ \t]+\\(\\w+\\)"
+"Regular expression matching the start of a method header.")
+
+;;method arguments
+(defconst *ojs-method-arguments-start*
+  "\\<method\\>\\([ \t]+\\w+\\)?[ \t]*\\<on\\>\\([ \t]+\\w+\\)?([ \t]*\\w")
+
+;; function definition
+(defconst *ojs-function-heading*
+"^[ \t]*function[ \t]+\\(\\w+\\)"
+"Regular expression matching the start of a function header.")
+
+;; function arguments
+(defconst *ojs-function-arguments-start*
+  "\\<function\\>\\([ \t]+\\w+\\)?[ \t]*([ \t]*\\w")
+
+;; function or method regexp
+(defconst *ojs-function-or-method-regexp*
+  "^[ \t]*\\(\\<function\\>\\|\\<method\\>[ \t]+\\w+[ \t]+\\<on\\>[ \t]+\\w+\\)([ \t]*\\w*)")
+
+(defconst *arguments-end*
+  "\\(\\w+\\)\\([ \t]*).*\\)?")
+
+;; vars definition
+(defconst *ojs-vars-regexp* 
+;;  "^.*var[ \t]+\\(\\w+\\)[ \t]*\\(=\\|in\\|;\\).*$")
+  "^.*var[ \t]+\\(\\w+\\)")
+
+;; new type(
+(defconst *ojs-new-type-regexp*
+  ".*new[ \t]+\\(\\w+\\)[ \t]*(")
+
+;; function starts
+(defconst *ojs-function-start-regexp*
+  "\\(function\\|method\\|on new\\|on modifyafter\\|on modifybefore\\|on delete\\)")
+
+;; kernel functions are in italic
+(defface ojs-kernel-functions-face
+    '((t 
+       :inherit font-lock-function-name-face :slant italic))
+    "OJS kernel fonts are displayed in italic"
+    )
+
+(defvar ojs-kernel-functions-face
+  'ojs-kernel-functions-face)
+
+;; variable defintion are in bold
+(defface ojs-var-definition-face
+    '((t 
+       :inherit font-lock-variable-name-face :weight bold))
+    "variable defintion are in bold"
+    )
+
+(defvar ojs-var-definition-face
+  'ojs-var-definition-face)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; highlight functions defined in the buffer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; highlight kernel functions 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;(defvar *ojs-kernel-functions-cache* nil)
+
+;;(defvar *ojs-kernel-functions-present* t)
+
+;;(defun list-ojs-kernel-functions ()
+;;  (cond (*ojs-kernel-functions-cache*
+;;	 *ojs-kernel-functions-cache*)
+;;	(*ojs-kernel-functions-present*
+;;	 (progn (setq *ojs-kernel-functions-present* (when (fi::lep-open-connection-p) (fi:eval-in-lisp "(if (fboundp 'jvs::list-all-js-functions) t nil)")))
+;;		(when *ojs-kernel-functions-present*
+;;		  (setq *ojs-kernel-functions-cache* (format "\\(%s\\)" (js--regexp-opt-symbol (when (fi::lep-open-connection-p) (fi:eval-in-lisp "(jvs::list-all-js-functions)"))))))
+;;		*ojs-kernel-functions-cache*))
+;;	(t
+;;	 nil)))
+
+;;(defun search-kernel-functions (end)
+;;  (when (list-ojs-kernel-functions)
+;;    (let ((search-pattern (list-ojs-kernel-functions)))
+;;      (re-search-forward search-pattern end t))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; highlight locally defined vars
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;use the ojs one
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; syntax hightlighting definition
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun setup-pjs-syntax-highlighting ()
+  ;; syntax highlighting for pjs
+  
+  (setq font-locks nil)
+
+  (setq font-lock-verbose t)
+
+  ;; list of font-lock-keywords in the right order
+
+  ;; Functions defined in buffers
+;;  (push (cons 'search-buffer-functions font-lock-function-name-face) font-locks)
+  (push (list 'search-buffer-functions 1 font-lock-function-name-face) font-locks)
+
+  ;; Kernel functions
+;;  (push (cons 'search-kernel-functions ojs-kernel-functions-face) font-locks)
+;;  (push (list 'search-kernel-functions 1 ojs-kernel-functions-face) font-locks)
+
+  ;; Variables in the function 
+  (push (cons 'search-vars-from-context font-lock-variable-name-face) font-locks)
+  ;; Global vars
+;;  (push (cons 'search-global-vars font-lock-variable-name-face) font-locks)
+  ;; Variable definitions
+  (push (list *ojs-vars-regexp* 1 ojs-var-definition-face) font-locks)
+
+  ;; New type
+  (push (list *ojs-new-type-regexp* 1 font-lock-type-face) font-locks)
+  ;; Function definition
+  (push (list *ojs-function-heading* 1 font-lock-function-name-face) font-locks)
+  ;; Function arguments
+  (push (list
+	 (concat *ojs-function-arguments-start*)
+	 (list *arguments-end*
+	       '(backward-char)
+	       '(end-of-line)
+	       '(1 ojs-var-definition-face))) font-locks)
+  ;; Method definition
+  (push (list *ojs-method-heading* 1 font-lock-function-name-face) font-locks)
+  (push (list *ojs-method-heading* 2 font-lock-keyword-face) font-locks)
+  (push (list *ojs-method-heading* 3 font-lock-type-face) font-locks)  
+  ;; Method arguments
+  (push (list
+	 (concat *ojs-method-arguments-start*)
+	 (list *arguments-end*
+	       '(backward-char)
+	       '(end-of-line)
+	       '(1 ojs-var-definition-face))) font-locks)
+  ;; keywords
+  (push (cons pjs-font-lock-keywords font-lock-keyword-face) font-locks)
+  ;; constants
+  (push (cons pjs-font-lock-constants font-lock-constant-face) font-locks)
+  ;; types
+  (push (cons pjs-font-lock-types font-lock-type-face) font-locks)
+  
+  ;; comments
+  (set (make-local-variable 'comment-start) "// ")
+  (set (make-local-variable 'comment-end) "")
+ 
+  (set (make-local-variable 'font-lock-defaults)
+       (list font-locks
+	     nil  ;; fontify strings and comments
+	     t    ;; case insensitive fontifying
+	     ))
+
+  ;; regexp to mark the beginning of a function
+;;  (setq defun-prompt-regexp *ojs-function-or-method-regexp*)
+
+  ;; fontify all the things
+  (syntax-propertize (point-max))
+  )
+
+(defun force-syntax-highlighting ()
+  (interactive)
+  (font-lock-fontify-buffer))
---
+++ el/pjs-mode.el
@@ -0,0 +1,142 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; syntax table
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'cc-mode)
+
+(defvar pjs-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    (c-populate-syntax-table table)
+    ;; The syntax class of underscore should really be `symbol' ("_")
+    ;; but that makes matching of tokens much more complex as e.g.
+    ;; "\\<xyz\\>" matches part of e.g. "_xyz" and "xyz_abc". Defines
+    ;; it as word constituent for now.
+    (modify-syntax-entry ?_ "w" table)
+;;    (modify-syntax-entry ?_ "_" table)
+    (modify-syntax-entry ?: "_" table)
+    (modify-syntax-entry ?- "_" table)
+    table)
+  "Syntax table used in JavaScript mode.")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; menu and keyboard shortcuts
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar *pjs-mode-map* (make-sparse-keymap))
+
+;; we don't want to stack definition declarations
+(setq fi:maintain-definition-stack nil)
+
+;; try to get a full symbol from the position given
+(defun find-symbol-at-point (start end)
+  ;; is the previous char a : ?
+  (save-excursion
+    (goto-char start)
+    (backward-char)
+    (when (looking-at ":")
+      (while (and (not (or (looking-at "[ (]")
+			   (looking-at "^")))
+		  (> (point) (point-min)))
+	(backward-char))
+      (if (looking-at "^")
+	  (setq start (point))
+	(setq start (1+ (point))))))
+  (buffer-substring-no-properties start end))
+    
+;; <ctrl-c .> in ojs file
+;; works with ojs and lisp file and properly do the search
+(defun %pjs-find-definition (tag)
+  (interactive
+   (if current-prefix-arg
+       '(t)
+     (list (car (fi::get-default-symbol "Lisp locate source" t t)))))
+  (if (string-match ":" tag)
+      (fi::lisp-find-definition-common tag nil)
+    (fi::lisp-find-definition-common (concat "js::" tag) nil)))
+  
+(defun %pjs-list-who-calls (tag)
+  (interactive
+   (if current-prefix-arg
+       '(nil)
+     (list (car (fi::get-default-symbol "Lisp locate source" t t)))))
+  (if (string-match ":" tag)
+      (fi:list-who-calls tag)
+    (fi:list-who-calls (concat "js::" tag))))
+
+(defvar *pjs-compilation-buffer-name* "*PJS compilation traces*")
+
+(defun trace-pjs-function(tag)
+  (interactive
+   (if current-prefix-arg
+       '(nil)
+     (list (car (fi::get-default-symbol "Lisp (un)trace function" t t)))))
+  (let ((js-symbol (concat "js::" tag)))
+    (fi:toggle-trace-definition js-symbol)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; new mode definition
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;(define-derived-mode opx2-js-mode prog-mode "OPX2 javascript"
+(define-derived-mode pjs-mode prog-mode "Planisware Script V2"
+  :syntax-table pjs-mode-syntax-table
+
+  ;; load a little bit of cc-mode for indentation
+  (c-initialize-cc-mode t)
+  (c-init-language-vars-for 'c-mode)
+  (c-common-init 'c-mode)
+
+  ;; set up syntax hightlighting
+  (setup-pjs-syntax-highlighting)
+
+  ;; custom keybindings from menu
+  (define-key *pjs-mode-map* "\C-c." '%ojs-find-definition)
+  (define-key *pjs-mode-map* "\C-c," 'fi:lisp-find-next-definition)
+  (define-key *pjs-mode-map* "\C-cc" '%ojs-list-who-calls)
+  (define-key *pjs-mode-map* "\C-ce" 'compile-ojs-file)
+  (define-key *pjs-mode-map* "\C-ck" 'check-ojs-region)
+  (define-key *pjs-mode-map* "\C-cr" 'compile-ojs-region)
+  (define-key *pjs-mode-map* "\C-c\C-b" 'save-and-compile-ojs-file)
+  (define-key *pjs-mode-map* "\C-cs" 'save-compile-and-sync-ojs-file)
+  (define-key *ojs-mode-map* "\C-ct" 'trace-ojs-function)
+
+  (define-key *pjs-mode-map* "\C-cl" 'lock-file)
+  (define-key *pjs-mode-map* "\C-cu" 'unlock-file)
+
+  ;; comment / un-comment
+  (define-key *pjs-mode-map* "\C-c;" 'comment-region)
+  (define-key *pjs-mode-map* "\C-c:" 'uncomment-region)
+
+  ;; autoindentation on new line and add a closing } if needed
+  (define-key *pjs-mode-map* (kbd "RET") 'newline-and-indent)
+;;  (define-key *ojs-mode-map* (kbd "RET") 'ojs-mode-insert-lcurly-on-ret)
+  ;; auto insert closing }
+;;  (define-key *ojs-mode-map* (kbd "{") 'ojs-mode-insert-lcurly)
+  
+  ;; menu
+  (easy-menu-define pjs-menu *pjs-mode-map* "Planisware Script Menu"
+    '("Planisware Script"
+      ["Compile and load file..." compile-ojs-file
+       t]
+      ["Check syntax of selected region" check-ojs-region
+       t]	    
+      ["Compile, load and synchronize file..." save-compile-and-sync-ojs-file
+       t]
+      ["Compile and run selected region" compile-ojs-region
+       t]	    
+      ["Find function definition..." %ojs-find-definition
+       t]
+      ["Trace/Untrace function..." trace-ojs-function
+       t]
+      ))
+
+  ;; custom keymap
+  (use-local-map *pjs-mode-map*)  
+  
+  ;; rebuild  function and vars cache on save and when we open a file
+;;  (add-hook 'after-save-hook 'pjs-reset-cache nil t)
+;;  (add-hook 'find-file-hook 'pjs-reset-cache nil t)
+  )
+
+;; pjs files are pjs modes
+(setq auto-mode-alist (cons '("\\.pjs" . pjs-mode) auto-mode-alist))
