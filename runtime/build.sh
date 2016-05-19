d3cf4923d6bc26a1d7907a1458e7eb34f8615165
Thibault Roche <thibault.roche@planisware.com>
2016-05-19 17:01:12 +0200

    use build number when possible

--- runtime/build.sh
+++ runtime/build.sh
@@ -1,7 +1,7 @@
 if [ -z "$releaseVersion" ]; then
     # incremental release
     make zip
-    scp zip/emacs-runtime-incremental-`git describe --tags`.zip buildfix@porthos:/u01/web/versions/emacs_runtime/emacs-runtime-dev.zip
+    scp zip/emacs-runtime-incremental-$BUILDNUMBER.zip buildfix@porthos:/u01/web/versions/emacs_runtime/emacs-runtime-dev.zip
 else
     VERSION=$releaseVersion make zip
     git tag $releaseVersion -m "Emacs runtime $releaseVersion released"
