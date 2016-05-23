5a11f8ae7eb1d5eb71e04bf874dccefdade0d942
Thibault Roche <thibault.roche@planisware.com>
2016-05-23 10:16:05 +0200

    Build fix

--- runtime/el/copyfiles.sh
+++ runtime/el/copyfiles.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 DEST_DIR=$1
-SRC_DIR=../../
-FILES=files
-DIRS=directories
+SRC_DIR=../
+FILES=el/files
+DIRS=el/directories
 EXTENSIONS="el txt"
+
+if [ -z $1 ] 
+then
+    echo "Destination dir missing";
+    exit -1;
+fi
+
+if [ ! -f Makefile ]
+then
+    echo "Makefile not found ! This script should be run in the runtime directory directly"
+    exit -1;
+fi
+
+mkdir -p $DEST_DIR
 
 # copy files
 while read LINE; do    
@@ -14,6 +28,9 @@ done < $FILES
 while read LINE; do
     mkdir -p $DEST_DIR$LINE
     for EXT in $EXTENSIONS; do
-        cp $SRC_DIR$LINE*.$EXT $DEST_DIR$LINE
+        if ls $SRC_DIR$LINE*.$EXT 1> /dev/null 2>&1; 
+        then
+            cp $SRC_DIR$LINE*.$EXT $DEST_DIR$LINE
+        fi
     done
 done < $DIRS
