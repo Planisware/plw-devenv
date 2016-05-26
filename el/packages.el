5f53247eed185036e33ff485addcdffff66cd37a
Thibault Roche <thibault.roche@planisware.com>
2016-05-26 10:20:22 +0200

    * check required version of packages (magit requires 24.4)

--- el/packages.el
+++ el/packages.el
@@ -6,7 +6,7 @@
          '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
-(defvar *plw-required-packages* '(magit
+(defvar *plw-required-packages* '((magit 24 4)
 				  auto-complete
 				  ;;newcomment
 				  fuzzy
@@ -15,9 +15,30 @@
 
 (defun plw-packages-installed-p ()
   (loop for p in *plw-required-packages*
-        when (not (package-installed-p p)) do (return nil)
-        finally (return t)))
+	when (not (package-installed-p (if (consp p) (car p) p))) do (return nil)
+	finally (return t)))
 
+(defun plw-install-package (p)
+  (cond ((symbolp p)
+	 (unless (package-installed-p p) (package-install p)))
+	((consp p)
+	 (cond ((and (>= emacs-major-version (second p))
+		     (>= emacs-minor-version (third p)))
+		(unless (package-installed-p (car p)) (package-install (car p))))
+	       ((y-or-n-p (format "You version of Emacs (%s.%s) does not match the requirement for the package %s, which requires version %s.%s. Do you want to continue without the package %s ?"
+				  emacs-major-version
+				       emacs-minor-version
+				       (car p)
+				       (second p)
+				       (third p)
+				       (car p)))
+		;; do nothing here
+		)
+	       (t
+		(error "Uncompatible version of Emacs (%s.%s), wanted at least %s.%s to use package %s"
+		       emacs-major-version emacs-minor-version
+		       (second p) (third p) (car p)))))))
+  
 (unless (plw-packages-installed-p)
   ;; check for new packages (package versions)
   (message "%s" "Refreshing package database...")
@@ -25,7 +46,7 @@
   (message "%s" "Done.")
   ;; install the missing packages
   (dolist (p *plw-required-packages*)
-    (when (not (package-installed-p p))
+    (when (not (package-installed-p (if (consp p) (car p) p)))
       (message "Installing package %s ..." p)
-      (package-install p)
+      (plw-install-package p)
       (message "Done."))))
