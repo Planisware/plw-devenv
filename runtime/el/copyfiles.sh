ea47e1e0bb081f940237ed5fc1ca518fd6ba6bef
Thibault Roche <thibault.roche@planisware.com>
2017-01-27 15:16:00 +0100

    do not copy full dirs, copy only file by file

--- runtime/el/copyfiles.sh
+++ runtime/el/copyfiles.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+## -*- coding: windows-1252 -*- 
+##  COPYRIGHT (C) PLANISWARE 2017
+##
+##  All Rights Reserved
+##
+##  This program and the information contained herein are confidential to
+##  and the property of PLANISWARE and are made available only to PLANISWARE
+##  employees for the sole purpose of conducting PLANISWARE business.
+##
+##**************************************************************************
 DEST_DIR=$1
 SRC_DIR=../
 FILES=el/files
-DIRS=el/directories
-EXTENSIONS="el txt"
 
 if [ -z $1 ] 
 then
@@ -21,16 +29,5 @@ mkdir -p $DEST_DIR
 
 # copy files
 while read LINE; do    
-    cp $SRC_DIR$LINE $DEST_DIR
+    cp --parents $SRC_DIR$LINE $DEST_DIR
 done < $FILES
-
-# copy directories
-while read LINE; do
-    mkdir -p $DEST_DIR$LINE
-    for EXT in $EXTENSIONS; do
-        if ls $SRC_DIR$LINE*.$EXT 1> /dev/null 2>&1; 
-        then
-            cp $SRC_DIR$LINE*.$EXT $DEST_DIR$LINE
-        fi
-    done
-done < $DIRS
