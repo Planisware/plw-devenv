7b0417ff90f45837bedb5f404c317d8671df28d5
Thibault Roche <thibault.roche@planisware.com>
2017-05-29 17:37:13 +0200

    * MIT header

--- runtime/src/emacs.el
+++ runtime/src/emacs.el
@@ -1,3 +1,8 @@
+;; -*- coding: windows-1252 -*- 
+;; COPYRIGHT (C) PLANISWARE 2017
+;; Distributed under the MIT License
+;; See accompanying file LICENSE file or copy at http://opensource.org/licenses/MIT
+
 (load (format "%seli/fi-site-init.el" *opx2-network-folder-work-path*))
 (load (format "%sdevenv/emacs-plw-ext.el" *opx2-network-folder-work-path*))
 (load (format "%sopx2-runtime.el" *opx2-network-folder-work-path*))
