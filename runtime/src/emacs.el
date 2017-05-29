127df1c09dab4feab35f0a480258168aa88ecac2
Thibault Roche <thibault.roche@planisware.com>
2017-05-29 15:15:56 +0200

    Correct paths

--- runtime/src/emacs.el
+++ runtime/src/emacs.el
@@ -1,3 +1,3 @@
 (load (format "%seli/fi-site-init.el" *opx2-network-folder-work-path*))
-(load (format "%sdevenv/emacs-plw.el" *opx2-network-folder-work-path*))
+(load (format "%sdevenv/emacs-plw-ext.el" *opx2-network-folder-work-path*))
 (load (format "%sopx2-runtime.el" *opx2-network-folder-work-path*))
